bd33365fa0a5a5c4adac2167682eb994aab046b7
Soumajit-Sadhukhan <sadhukhansoumajit205@gmail.com>
2026-07-03 16:23:38 +0530

    first check backend

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+class HealthCheckView(APIView):
+    def get(self, rewuest):
+        return Response({
+            "status": "success",
+            "massage": "InsightFlow AI Backend is Running 🚀"
+        })
